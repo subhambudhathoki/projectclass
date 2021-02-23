f19bc936e153dec89cd5ad9cab7249592975c4db
Stark Industries <subhambudhathoki007@gmail.com>
2021-02-23 09:29:49 +0545

    Initial commit

---
+++ lab_exercise/sum_of_numbers.py
@@ -0,0 +1,5 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+sum=a+b+c
+print("the sum is",sum)
---
+++ main.py
@@ -0,0 +1,2 @@
+a=int(input("enter first number" ))
+b=int(input )
